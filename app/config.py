3f95766869ae9af887a362b4ee082c024dd72124
Olha Santo <olha.santo@gmail.com>
2025-12-13 12:08:29 -0600

    add Docker and docker-compose setup for local development

--- app/config.py
+++ app/config.py
@@ -1,16 +1,18 @@
+import os
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
+    database_username: str
+    database_password: str
     database_hostname: str
     database_port: str
-    database_password: str
     database_name: str
-    database_username: str
     secret_key: str
     algorithm: str
     access_token_expire_minutes: int
 
     class Config:
-        env_file = ".env"
+        env_file = os.getenv("ENV_FILE", ".env.local")
+        env_file_encoding = "utf-8"
 
 settings = Settings()
